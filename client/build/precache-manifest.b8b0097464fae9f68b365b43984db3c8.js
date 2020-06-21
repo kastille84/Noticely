f266001fe5d7281c34785a73838231125480d731
Edwin <Edwin.Martinez@people10.com>
2020-06-20 23:09:25 -0400

    fixed gitignore in client

---
+++ client/build/precache-manifest.b8b0097464fae9f68b365b43984db3c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56d8baa5a4e730d49830e486170c1d77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db178d184088584d07ff",
+    "url": "/static/css/2.975c484c.chunk.css"
+  },
+  {
+    "revision": "c035f5adcbd23fdbccab",
+    "url": "/static/css/main.84927359.chunk.css"
+  },
+  {
+    "revision": "db178d184088584d07ff",
+    "url": "/static/js/2.19acf33b.chunk.js"
+  },
+  {
+    "revision": "2f44b3e8b395d2bb2932ad00db6a4c2d",
+    "url": "/static/js/2.19acf33b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c035f5adcbd23fdbccab",
+    "url": "/static/js/main.ee26d5e4.chunk.js"
+  },
+  {
+    "revision": "b733ce12e3d6e59d9bb0",
+    "url": "/static/js/runtime-main.89a8efe6.js"
+  }
+]);
